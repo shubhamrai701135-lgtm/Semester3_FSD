c6071070bc91ee14abc2ae3ab48cca6b2d781979
shubham kumsr rai <shubhamrai701135@gmail.com>
2026-09-17 11:26:20 +0530

    Add exp2.js

---
+++ exp2.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+
+fs.writeFileSync('student.txt', 'B.Tech Node.js Lab');
+
+
+const data = fs.readFileSync('student.txt', 'utf8');
+console.log(data);
+
+
+fs.writeFileSync(
+    'student.txt',
+    'Name: Rahul\nSubject: Full Stack Development'
+);
+
+console.log("File created successfully");
+
+fs.appendFileSync(
+    'student.txt',
+    '\nExperiment 2 completed.'
+);
